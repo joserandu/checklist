ade303b81df4193d1cee6e563eb5d612f1892678
diegorbastos <b.diieego@gmail.com>
2025-07-10 17:02:16 -0300

    test added

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,10 @@ module checklist {
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
+    requires junit;
 
     exports view;
+
+    opens test to junit;
 }
